0d48276d6df96acf5fd8612daaea18f60b3e154e
Adriano Xavier <aj-dx@hotmail.com>
2022-06-20 20:31:40 -0300

    Alterando respostas

--- server.js
+++ server.js
@@ -1,17 +1,39 @@
 const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
-const init = "<html><body><h3>Hello Word!</h3></body></html>";
-const teste = "<html><body><p>Isso é um teste!</p></body>";
+
+
 
 http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-
+    var title = '';
+    var content = '';
     if (req.url == "/teste") {
-        res.end(teste);
+        title = "Teste";
+        content = "Isso é um teste!"
     } else {
-        res.end(init);
+        title = "Hello";
+        content = "Hello World"
     }
+
+    var code = [
+        '<!DOCTYPE html>',
+        '<html>',
+        '<head>',
+        '<meta charset="utf-8" />',
+        '<title>' + title + '</title>',
+        '</head>',
+        '<body>',
+        '<h3>',
+        content,
+        '</h3>',
+        '</body>',
+        '</html>'
+    ].join('\n');
+
+    res.write(code, "utf8");
+    res.end();
+    
 }).listen(port, hostname, () => {
     console.log("O servidor está sendo executado en http://127.0.0.1:3000/");
 });
